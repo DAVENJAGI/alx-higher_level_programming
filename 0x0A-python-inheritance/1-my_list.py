d4275d8c956557f895a1f57454011f5cda5f2999
DAVENJAGI <njagidave39@gmail.com>
2023-07-10 06:42:58 -0700

    Inheritance) \

---
+++ 0x0A-python-inheritance/1-my_list.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+"""Class called MyList"""
+
+class MyList(list):
+
+    """Defines public attribute sorted"""
+    def print_sorted(self):
+        """prints sorted"""
+        print(sorted(self))
