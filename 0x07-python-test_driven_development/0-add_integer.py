8f8afb4f77667661e6d75112cf2d0074ad53b8bd
DAVENJAGI <njagidave39@gmail.com>
2023-09-17 19:21:43 -0400

    TDD

--- 0x07-python-test_driven_development/0-add_integer.py
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -1,11 +1,21 @@
 #!/usr/bin/python3
+"""defines a function
+
+    Function takes two integers and adds them
+
+"""
+
+
 def add_integer(a, b=98):
+    """
+    adds two integers
+    """
     if not isinstance(a, (int, float)):
         raise TypeError("a must be an integer")
     if not isinstance(b, (int, float)):
         raise TypeError("b must be an integer")
 
-    a = int(a)
-    b = int(b)
+#    a = int(a)
+#    b = int(b)
 
-    return (int(a + b))
+    return (int(a) + int(b))
