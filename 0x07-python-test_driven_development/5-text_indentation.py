0de31135ad818bbb8f8d6f7058a1c1621d4d23c0
DAVENJAGI <njagidave39@gmail.com>
2023-09-21 09:41:09 -0400

    TDD

--- 0x07-python-test_driven_development/5-text_indentation.py
+++ 0x07-python-test_driven_development/5-text_indentation.py
@@ -5,8 +5,12 @@
 
 
 def text_indentation(text):
+    """Prints two lines after the characters .? and :
+    Args: text(str) - text to be printed
+    raise a TypeError if text is not a string
+    """
     if not isinstance(text, str):
         raise TypeError("text must be a string")
     for char in ".?:":
         text = text.replace(char, char + "\n\n")
-    print(text, end="\n")
+    print(text, end="")
