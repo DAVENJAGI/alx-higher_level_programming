9a401a35a4df336ce7dea89b261e4b6e9eecb2e0
DAVENJAGI <njagidave39@gmail.com>
2023-10-17 09:17:14 -0400

    DBMS

--- 0x0D-SQL_introduction/10-top_score.sql
+++ 0x0D-SQL_introduction/10-top_score.sql
@@ -1,4 +1,6 @@
 -- List all records script.
+-- Records should be ordered by score (top first)
+-- The database name will be passed as an argument of the mysql command
 SELECT 'score', 'name'
 FROM 'second_table'
 ORDER BY 'score' DESC;
