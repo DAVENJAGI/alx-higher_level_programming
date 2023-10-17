54a2e24010490aac91637364fc76098fde8f1d0c
DAVENJAGI <njagidave39@gmail.com>
2023-10-17 09:50:14 -0400

    DBMS

--- 0x0D-SQL_introduction/7-insert_value.sql
+++ 0x0D-SQL_introduction/7-insert_value.sql
@@ -1,3 +1,3 @@
 -- insert first value
 -- New row id = 89 name = Best School
-INSERT INTO 'first_table'('id', 'name') VALUES (89, "Best School");
+INSERT INTO first_table (id, name) VALUES (89, "Best School");
