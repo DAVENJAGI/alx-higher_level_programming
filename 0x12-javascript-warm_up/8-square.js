bcc7071ea62ddedec34bcc25f32441859691a67c
DAVENJAGI <njagidave39@gmail.com>
2023-11-13 10:57:27 -0500

    JavaScript

--- 0x12-javascript-warm_up/8-square.js
+++ 0x12-javascript-warm_up/8-square.js
@@ -6,7 +6,5 @@ if (isNaN(size)) {
   console.log('Missing size');
 } else {
   const row = 'X'.repeat(size)
-  for (let i = 0; i < size; i++) {
-    console.log(row);
-  }
+  for (let i = 0; i < size; i++) console.log(row);
 }
