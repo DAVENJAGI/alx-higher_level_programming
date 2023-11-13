a4e19a20e0b4cf8b5e0a69f5c64b6078ce1a71c0
DAVENJAGI <njagidave39@gmail.com>
2023-11-13 09:42:06 -0500

    JavaScript

--- 0x12-javascript-warm_up/2-arguments.js
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -4,8 +4,10 @@ const args = process.argv.length;
 
 if (args === 2) {
 	console.log('No argument');
-} else if (args === 3) {
+}
+else if (args === 3) {
 	console.log('Argument found');
-} else {
+}
+else {
 	console.log('Arguments found');
 }
