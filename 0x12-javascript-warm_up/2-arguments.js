07003f7fe7862b7ad6b04783c81394d541544127
DAVENJAGI <njagidave39@gmail.com>
2023-11-13 09:58:42 -0500

    JavaScript

--- 0x12-javascript-warm_up/2-arguments.js
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -1,7 +1,8 @@
 #!/usr/bin/node
 
 const args = process.argv.length;
-//control statements
+
+//check the length of the process
 if (args === 2) {
   console.log('No argument');
 } else if (args === 3) {
