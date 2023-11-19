b3bb180ca2e2d1b62a4814cad6a4d56329294d08
DAVENJAGI <njagidave39@gmail.com>
2023-11-19 10:02:55 -0500

    SQLAlchemy

--- 0x0F-python-object_relational_mapping/1-filter_states.py
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -4,15 +4,13 @@ Script to list all states with name starting with uppercase N
 from database.
 """
 
-from MySQLdb import mysql
-from sys import argv
-
+import MySQLdb
+import sys
 if __name__ == "__main__":
-    db = MySQLdb.connect(host="127.0.0.1", user=sys.argv[1],
+    db = MySQLdb.connect(host="localhost", user=sys.argv[1],
                          passwd=sys.argv[2], db=sys.argv[3], port=3306)
-
     cur = db.cursor()
-    cur.execute("SELECT * FROM states WHERE name LIKE 'N%' ORDERD BY id ASC")
+    cur.execute("SELECT * FROM states WHERE name LIKE 'N%' ORDER BY id ASC")
     rows = cur.fetchall()
     for record in rows:
         print(record)
