e9fca5ce0e24bc6df1a1c7cdf873862914b105ed
DAVENJAGI <njagidave39@gmail.com>
2023-11-19 11:42:57 -0500

    SQLAlchemy

--- 0x0F-python-object_relational_mapping/2-my_filter_states.py
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -9,7 +9,7 @@ import MySQLdb
 import sys
 
 if __name__ == "__main__":
-    db = MySQLdb.connect(host="127.0.0.1", user=sys.argv[1],
+    db = MySQLdb.connect(host="localhost", user=sys.argv[1],
                          passwd=sys.argv[2], db=sys.argv[3], port=3306)
     cur = db.cursor()
     cur.execute("SELECT * FROM states WHERE name LIKE '{:s}' ORDER BY \
@@ -18,7 +18,7 @@ if __name__ == "__main__":
     rows = cur.fetchall()
     
     for record in rows:
-        if record[1] == sys.argv[4]:
-            print(record)
+        print(record)
+
     cur.close()
     db.close()
