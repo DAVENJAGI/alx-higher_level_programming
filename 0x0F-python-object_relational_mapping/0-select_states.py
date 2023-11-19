4a7435eff78090cf1af06e17704d49fac411df3f
DAVENJAGI <njagidave39@gmail.com>
2023-11-19 10:15:10 -0500

    SQLAlchemy

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -6,13 +6,13 @@ import sys
 if __name__ == "__main__":
     db = MySQLdb.connect(host="127.0.0.1", user=sys.argv[1],
                          passwd=sys.argv[2], db=sys.argv[3], port=3306)
-    cursor = db.cursor()
-    cursor.execute("SELECT * FROM states ORDER BY id ASC")
+    cur = db.cursor()
+    cur.execute("SELECT * FROM states ORDER BY id ASC")
 
-    rows = cursor.fetchall()
+    rows = cur.fetchall()
 
     for record in rows:
         print(record)
 
-    cursor.close()
+    cur.close()
     db.close()
