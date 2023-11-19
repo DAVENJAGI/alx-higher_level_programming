0d28239a18065dd7358177c63a44d64196465437
DAVENJAGI <njagidave39@gmail.com>
2023-11-19 12:46:55 -0500

    SQLAlchemy

--- 0x0F-python-object_relational_mapping/4-cities_by_state.py
+++ 0x0F-python-object_relational_mapping/4-cities_by_state.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 """
-Lists all cities from database hbtni_0e_o_usa
+Lists all cities from database hbtn_0e_o_usa
 """
 
 import MySQLdb
@@ -9,7 +9,7 @@ if __name__ == "__main__":
     db = MySQLdb.connect(host="localhost", user=sys.argv[1],
                          passwd=sys.argv[2], db=sys.argv[3], port=3306)
     cur = db.cursor()
-    cur.execute("SELECT * FROM state.cities ORDER BY id ASC")
+    cur.execute("SELECT * FROM state.cities ORDER BY cities.id ASC")
 
     rows = cur.fetchall()
 
