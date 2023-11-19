9dbaf8eaff9d382d3e8b6e11aa711f1467103d11
DAVENJAGI <njagidave39@gmail.com>
2023-11-19 16:53:09 -0500

    SQLAlchemy

--- 0x0F-python-object_relational_mapping/2-my_filter_states.py
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -10,12 +10,13 @@ import sys
 if __name__ == "__main__":
     dbm = db.connect(host="localhost", port=3306,
                      user=argv[1], passwd=argv[2], db=argv[3])
+    
     cur = dbm.cursor()
-
     cur.execute(
         "SELECT * FROM states WHERE name LIKE BINARY '{}' ORDER BY \
                         states.id ASC".format(argv[4]))
-    rows = dbm.fetchall()
+    
+    rows = cur.fetchall()
 
     for row in rows:
         print(row)
