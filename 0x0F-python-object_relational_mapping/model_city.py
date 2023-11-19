958f6ba87a709c09f65bc4e8d49cc585401389e7
DAVENJAGI <njagidave39@gmail.com>
2023-11-19 16:25:22 -0500

    SQLAlchemy

--- 0x0F-python-object_relational_mapping/model_city.py
+++ 0x0F-python-object_relational_mapping/model_city.py
@@ -20,4 +20,3 @@ class City(Base):
     id = Column(Integer, primary_key=True, auto_increment=True, nullable=False)
     name = Column(String(128), nullable=False)
     state_id = Column(Integer, ForeignKey("state.id"), nullable=False)
-
