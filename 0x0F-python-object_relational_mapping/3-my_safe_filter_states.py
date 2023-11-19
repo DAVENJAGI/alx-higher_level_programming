abd4d42171beb6f1c9f03d8de80508ca10f3df1f
DAVENJAGI <njagidave39@gmail.com>
2023-11-19 16:51:08 -0500

    SQLAlchemy

--- 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
+++ 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
@@ -15,7 +15,7 @@ if __name__ == "__main__":
     from the database.
     """
     dbm = db.connect(host="localhost", port=3306,
-                            user=argv[1], passwd=argv[2], db=argv[3])
+                     user=argv[1], passwd=argv[2], db=argv[3])
 
     cur = dbm.cursor()
     cur.execute(
