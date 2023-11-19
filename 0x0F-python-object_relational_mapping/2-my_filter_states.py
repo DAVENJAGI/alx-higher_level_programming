57fc448d268780c6107c437c6411e5b7aeca3dc1
DAVENJAGI <njagidave39@gmail.com>
2023-11-19 16:55:46 -0500

    SQLAlchemy

--- 0x0F-python-object_relational_mapping/2-my_filter_states.py
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -8,15 +8,15 @@ import MySQLdb as db
 import sys
 
 if __name__ == "__main__":
-    dbm = db.connect(host="localhost", port=3306,
-                     user=argv[1], passwd=argv[2], db=argv[3])
-    
-    cur = dbm.cursor()
-    cur.execute(
+    db_connect = db.connect(host="localhost", port=3306,
+                            user=argv[1], passwd=argv[2], db=argv[3])
+    db_cursor = db_connect.cursor()
+
+    db_cursor.execute(
         "SELECT * FROM states WHERE name LIKE BINARY '{}' ORDER BY \
                         states.id ASC".format(argv[4]))
-    
-    rows = cur.fetchall()
+    rows_selected = db_cursor.fetchall()
 
-    for row in rows:
+    for row in rows_selected:
         print(row)
+  
