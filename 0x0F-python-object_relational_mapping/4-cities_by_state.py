1e29a10c03c1a3faaf340dd14ba6a724cb8aaefb
DAVENJAGI <njagidave39@gmail.com>
2023-11-19 12:36:53 -0500

    SQLAlchemy

--- 0x0F-python-object_relational_mapping/4-cities_by_state.py
+++ 0x0F-python-object_relational_mapping/4-cities_by_state.py
@@ -10,10 +10,7 @@ if __name__ == "__main__":
                          passwd=sys.argv[2], db=sys.argv[3], port=3306)
     cur = db.cursor()
     cur.execute("""
-                SELECT cities.id, cities.name, state.name
-                FROM cities JOIN states
-                ON cities.state_id = state.id
-                ORDER BY cities.id ASC
+                SELECT * FROM state.cities ORDER BY cities.id ASC
                 """)
 
     rows = cur.fetchall()
