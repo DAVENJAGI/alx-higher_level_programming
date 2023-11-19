5b789813ef1895a76e26a4c8634c6d128f927d9e
DAVENJAGI <njagidave39@gmail.com>
2023-11-19 16:20:36 -0500

    SQLAlchemy

--- 0x0F-python-object_relational_mapping/model_city.py
+++ 0x0F-python-object_relational_mapping/model_city.py
@@ -6,7 +6,7 @@ and an instance Base = declarative_base()
 
 
 from sqlalchemy import Column, Integer, String, ForeignKey
-from sqlalchemy.ext import declarative_base
+from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy import create_engine
 from model_state import Base
 
