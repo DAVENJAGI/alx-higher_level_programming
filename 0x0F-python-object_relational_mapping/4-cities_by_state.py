49f62cc6027548364a169f5ddd7f4e184496e54b
DAVENJAGI <njagidave39@gmail.com>
2023-11-19 12:31:37 -0500

    SQLAlchemy

--- 0x0F-python-object_relational_mapping/4-cities_by_state.py
+++ 0x0F-python-object_relational_mapping/4-cities_by_state.py
@@ -9,14 +9,18 @@ if __name__ == "__main__":
     db = MySQLdb.connect(host="localhost", user=sys.argv[1],
                          passwd=sys.argv[2], db=sys.argv[3], port=3306)
     cur = db.cursor()
-    cur.execute(" SELECT cities.id, cities.name, state.name
-                FROM cities INNER JOIN states
-                ON cities.state_id=state.id ORDER BY cities.id")
+    cur.execute("""
+                SELECT cities.id, cities.name, state.name
+                FROM cities JOIN states
+                ON cities.state_id = state.id
+                ORDER BY cities.id ASC
+                """)
 
     rows = cur.fetchall()
 
-    for record in rows:
-        print(record)
+    if row is not None:
+        for record in rows:
+            print(record)
 
     cur.close()
     db.close()
