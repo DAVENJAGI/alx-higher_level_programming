5697cf41e25f9f0e89c0093a4f1e3fdae0160459
DAVENJAGI <njagidave39@gmail.com>
2023-11-19 14:01:58 -0500

    SQLAlchemy

--- 0x0F-python-object_relational_mapping/5-filter_cities.py
+++ 0x0F-python-object_relational_mapping/5-filter_cities.py
@@ -6,12 +6,10 @@ import MySQLdb
 import sys
 
 if __name__ == "__main__":
-    db = MySQLdb.connect(host="localhost", user=sys.argv[1],
-                         passwd=sys.argv[2], db=sys.argv[3], port=3306)
-    cur = db.cursor()
-    cur.execute("SELECT * FROM 'cities' as 'c' \
-                INNER JOIN 'states' as 's' \
-                ON 'c'.'state_id' = 's'.'id' \
-                ORDER BY 'c'.'id'")
-
+    db = MySQLdb.connect(user=sys.argv[1], passwd=sys.argv[2], db=sys.argv[3])
+    c = db.cursor()
+    c.execute("SELECT * FROM `cities` as `c` \
+                INNER JOIN `states` as `s` \
+                   ON `c`.`state_id` = `s`.`id` \
+                ORDER BY `c`.`id`")
     print(", ".join([ct[2] for ct in c.fetchall() if ct[4] == sys.argv[4]]))
