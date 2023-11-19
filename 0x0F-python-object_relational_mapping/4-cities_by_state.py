c38e7c94c344d8c2e445f90f89c2dc7aca6e029c
DAVENJAGI <njagidave39@gmail.com>
2023-11-19 13:21:33 -0500

    SQLAlchemy

--- 0x0F-python-object_relational_mapping/4-cities_by_state.py
+++ 0x0F-python-object_relational_mapping/4-cities_by_state.py
@@ -2,21 +2,15 @@
 """
 Lists all cities from database hbtn_0e_o_usa
 """
-
 import MySQLdb
 import sys
+
 if __name__ == "__main__":
-    db = MySQLdb.connect(host="localhost", user=sys.argv[1],
-                         passwd=sys.argv[2], db=sys.argv[3], port=3306)
+    db = MySQLdb.connect(user=sys.argv[1], passwd=sys.argv[2], db=sys.argv[3])
     cur = db.cursor()
-    cur.execute("SELECT cities.id, cities.name, states.name \
-                FROM cities JOIN states ON cities.state_id = state.id;")
-
-    rows = cur.fetchall()
-
-    if row is not None:
-        for record in rows:
-            print(record)
-
-    cur.close()
-    db.close()
+    cur.execute("SELECT `c`.`id`, `c`.`name`, `s`.`name` \
+                 FROM `cities` as `c` \
+                INNER JOIN `states` as `s` \
+                   ON `c`.`state_id` = `s`.`id` \
+                ORDER BY `c`.`id`")
+    [print(city) for city in c.fetchall()]
