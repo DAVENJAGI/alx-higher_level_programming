a811e76baa6de408bbe3c9c6f010b958fbeeb3f6
DAVENJAGI <njagidave39@gmail.com>
2023-12-01 18:22:15 -0500

    Python_Networking_1

---
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""fetches http:??alx-intranet.hbtn.io"""
+import urllib.request
+
+if __name__ == "__main":
+    with urllib.request.urlopen ("https://alx-intranet.hbtn.io/status") as n:
+        cont = n.read()
+        print("Body response:")
+        print("\t - type: {}".format(type(cont)))
+        print("\t - content: {}".format(cont))
+        print("\t - utf8 content: {}".format(content.decode('utf-8')))
