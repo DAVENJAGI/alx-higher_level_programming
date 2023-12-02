f9ef8d68f6d6b2da5b52a1fd2d6219b2488f1d0e
DAVENJAGI <njagidave39@gmail.com>
2023-12-01 21:03:50 -0500

    Python_Networking_1

---
+++ 0x11-python-network_1/6-post_email.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""
+Python script that takes in a URL and an email,
+sends a POST request to the passed URL with the email as a parameter,
+and displays the body of the response
+"""
+
+if __name__ == "__main__":
+    import requests
+    import sys
+
+    hbtn = request.post(sys.argv[1], data={'email': sys.argv[2]})
+    print(hbtn.text)
