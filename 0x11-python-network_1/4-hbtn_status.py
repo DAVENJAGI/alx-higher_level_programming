db74962037b6d56afdc5099f7f00d803fdb5f435
DAVENJAGI <njagidave39@gmail.com>
2023-12-01 19:55:01 -0500

    Python_Networking_1

---
+++ 0x11-python-network_1/4-hbtn_status.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""
+fetches http://alx-intranet.hbtn.io
+ must use packageurllib
+"""
+
+
+if __name__ == "__main__":
+    import requests
+
+    with requests.get('https://alx-intranet.hbtn.io/status') as hbtn:
+        cont = hbtn.text
+        print("Body response:")
+        print("\t- type: {}".format(type(cont)))
+        print("\t- content: {}".format(cont))
+        print("\t- utf8 content: {}".format(cont))
