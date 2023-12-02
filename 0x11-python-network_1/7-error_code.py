021c9c3fdf2d623edc2cee30b3b4195ba3be57f8
DAVENJAGI <njagidave39@gmail.com>
2023-12-01 21:21:26 -0500

    Python_Networking_1

--- 0x11-python-network_1/7-error_code.py
+++ 0x11-python-network_1/7-error_code.py
@@ -9,7 +9,7 @@ import sys
 
 if __name__ == "__main__":
     hbtn = requests.get(sys.argv[1])
-    if hbtn >= 400:
+    if hbtn.status_code >= 400:
         print('Error code: {}'.format(hbtn.statu_code))
     else:
         print(hbtn.text)
