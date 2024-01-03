341331becea1385dea651df4f2cbf20accc363b4
DAVENJAGI <njagidave39@gmail.com>
2024-01-03 17:34:15 +0300

    Web_Scrapping_task_2

--- 0x14-javascript-web_scraping/2-statuscode.js
+++ 0x14-javascript-web_scraping/2-statuscode.js
@@ -5,8 +5,8 @@ const url = process.argv[2];
 
 request(url, (error, response, body) => {
   if (error) {
-    console.error('code: ', response.statusCode);
+    console.error('code:', response.statusCode);
   } else {
-    console.log('code: ', response.statusCode);
+    console.log('code:', response.statusCode);
   }
 });
