9106781ffd16a235d018907375a7cb586290067e
DAVENJAGI <njagidave39@gmail.com>
2024-01-04 02:22:17 +0300

    API_Advanced_Task

--- 0x14-javascript-web_scraping/101-starwars_characters.js
+++ 0x14-javascript-web_scraping/101-starwars_characters.js
@@ -14,18 +14,21 @@ request(requestUrl, (error, response, body) => {
   } else {
     const data = JSON.parse(body);
     const filmCharacter = data.characters;
-    const characters = [];
+    const list = [];
     let characterFetched = 0;
 
     filmCharacter.forEach(characterUrl => {
       request(characterUrl, (error, response, body) => {
         if (!error && response.statusCode === 200) {
           const character = JSON.parse(body);
-          characters.push(character.name);
+	  console.log('good job');
+          list.push(character.name);
 	  characterFetched++;
+
 	  if (characterFetched === filmCharacter.length) {
-            characters.forEach(name => console.log(name));
-          } else {
+	  console.log(name);
+//            characters.forEach(name => console.log(name));
+	  } else {
             console.error('Error getting character:', error);
           }
         }
